c5befec55fda2633ae36fd2cfeb17de55f3f9932
SerhiySBV <serhiybelyavskii@gmail.com>
2023-12-11 14:45:16 +0200

    Edit tsconfig

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-var button = document.querySelector("button");
-var input1 = document.getElementById("num1");
-var input2 = document.getElementById("num2");
+"use strict";
+const button = document.querySelector("button");
+const input1 = document.getElementById("num1");
+const input2 = document.getElementById("num2");
 function add(num1, num2) {
     return num1 + num2;
 }
 button.addEventListener("click", function () {
     console.log(add(+input1.value, +input2.value));
 });
-var age;
-var firstName;
-var toggle;
-var empty;
-var notInitialize;
-var callback = function (a) { return 100 + a; };
+let age;
+let firstName;
+let toggle;
+let empty;
+let notInitialize;
+let callback = (a) => { return 100 + a; };
 age = 50;
 firstName = 'Max';
 toggle = true;
 empty = null;
 notInitialize = undefined;
-callback = function (a) { return 100 + a; };
+callback = (a) => { return 100 + a; };
+//# sourceMappingURL=app.js.map
